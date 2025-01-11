a6aaa482f1ad84bba54066af64ecaac0c63b0791
alisitki <alisitkia@gmail.com>
2025-01-11 11:42:10 +0300

    araba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-// pages/index.tsx
-
 import React from "react";
 import Image from "next/image";
 import { FaWhatsapp, FaPhone } from "react-icons/fa";
 import "@fontsource/montserrat";
 
-
-
 const HomePage: React.FC = () => {
   return (
-    <div
-      className="min-h-screen w-full  flex flex-col items-center py-8 px-4 relative"
-    >
+    <div className="min-h-screen w-full flex flex-col items-center py-8 px-4 relative">
       {/* Background Image */}
       <div
         className="absolute inset-0 bg-[url('/bg2.jpg')] bg-cover bg-center opacity-80 -z-20"
@@ -19,17 +13,14 @@ const HomePage: React.FC = () => {
 
       {/* Header */}
       <header className="w-full max-w-4xl text-center mb-8">
-      <h1 className="text-4xl font-extrabold text-white animate-glow font-sans">
-  Kurye Hattı
-</h1>
-
-
-
+        <h1 className="text-4xl font-extrabold text-white animate-glow font-sans">
+          Kurye Hattı
+        </h1>
 
         <p className="text-lg text-white mt-3 max-w-2xl mx-auto leading-7">
-        &ldquo;Hızlı, güvenilir ve profesyonel kurye hizmetimizle gönderileriniz
-  her zaman güvende. En hızlı teslimat için hemen bizimle iletişime
-  geçin.&rdquo;
+          &ldquo;Hızlı, güvenilir ve profesyonel kurye hizmetimizle gönderileriniz
+          her zaman güvende. En hızlı teslimat için hemen bizimle iletişime
+          geçin.&rdquo;
         </p>
       </header>
 
@@ -77,18 +68,30 @@ const HomePage: React.FC = () => {
         </a>
       </div>
 
-      {/* Ek Bilgi (opsiyonel) */}
+      <div className="relative w-full max-w-3xl h-96 mb-8 mt-10 rounded-md overflow-hidden shadow-lg ">
+        <Image
+          src="/araba2.jpg" // Kendi görselinizin yolunu düzenleyin
+          alt="Ticari Araç"
+          layout="fill"
+          objectFit="cover"
+          className="transition-transform duration-300 ease-in-out hover:scale-105"
+        />
+      </div>
+
+      {/* Harita */}
       <section className="w-full max-w-4xl mt-12">
         <div className="bg-white rounded-lg shadow p-6 text-center">
-          <h3 className="text-xl font-medium text-gray-800 mb-2">
-            Neden Bizi Tercih Etmelisiniz?
-          </h3>
-          <ul className="text-gray-700 leading-7 list-inside list-disc text-left max-w-md mx-auto">
-          <li className="mb-1">7/24 hızlı ve güvenilir teslimat</li>
-  <li className="mb-1">Uygun fiyatlandırma ve şeffaf süreç</li>
-  <li className="mb-1">Güler yüzlü ve deneyimli kurye ekibi</li>
-  <li className="mb-1">Tüm Türkiye&apos;ye geniş teslimat ağı</li>
-          </ul>
+          <h3 className="text-xl font-medium text-gray-800 mb-4">Konumumuz</h3>
+          <div className="w-full h-96 overflow-hidden rounded-md">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d634.0505404353444!2d29.189148178710223!3d40.89228690868145!3m2!1i1024!2i768!4f13.1!5e0!3m2!1str!2str!4v1736584159448!5m2!1str!2str"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen={true}
+              loading="lazy"
+            ></iframe>
+          </div>
         </div>
       </section>
 
